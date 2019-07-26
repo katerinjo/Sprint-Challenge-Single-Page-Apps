e7ea065258ec3a5b5eb7ea4bb1986d36662e6ea8
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-26 09:32:48 -0700

    locations show

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -34,5 +34,4 @@ export default function CharacterList() {
         ))}
       </Card.Group>
     </section>
-
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import { Card } from "semantic-ui-react";
 
-export default function LocationsList() {
+export default function CharacterList() {
+  // TODO: Add useState to track data from useEffect
+  const [locations, setLocations] = useState([]);
 
+  useEffect(() => {
+    // TODO: Add AJAX/API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get("https://rickandmortyapi.com/api/location/3,21")
+      .then(res => {
+        setLocations(res.data);
+      })
+      .catch(console.log);
+  }, [])
+
+  return <section className='location-list grid-view'>
+      <Card.Group>
+        {locations.map(location => (
+          <Card 
+            header={location.name}
+            meta={location.type}
+            description={`Dimension: ${location.dimension}`}
+          />
+        ))}
+      </Card.Group>
+    </section>
 }
