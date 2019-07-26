fa0ae9346872529ae356055f14dc2835ca3686c1
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-26 08:52:24 -0700

    install dependencies; successful ajax request

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get("https://rickandmortyapi.com/api/character/1,183")
+      .then(console.log)
+      .catch(console.log);
   }, [])
 
   return <section className='character-list grid-view'>
