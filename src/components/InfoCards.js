c23a4a903049921c5f88b94968282c8c5fa2a566
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-26 10:32:54 -0700

    first attempt at general card component

---
+++ src/components/InfoCards.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Card, Image } from "semantic-ui-react";
+
+export default function InfoCards({
+  data, header, image, meta,
+  detailNames = [], detailFuns = [] }) {
+  return (
+    <Card.Group>
+      {data.map(item => (
+        <Card>
+          <Card.Description>
+            {[character.type && <p>Type: {character.type}</p>,
+            character.gender && <p>Gender: {character.gender}</p>,
+            character.status && <p>Status: {character.status}</p>,
+            character.origin && <p>Origin: {character.origin.name}</p>]}
+          </Card.Description>
+          {[
+            item[image] && <Image src={item[image]} />,
+            item[header] && <Card.Header>{item[header]}</Card.Header>,
+            item[meta] && <Card.Meta>{item[meta]}</Card.Meta>,
+            detailNames.length > 0 && <Card.Description>
+              {detailNames.map((key, index) => {
+                const shallow = item[key];
+                const value = typeof shallow === "string" ? shallow : shallow.name;
+                return <p>{detailFuns[index](value)}</p>;
+              })}
+            </Card.Description>
+          ]}
+        </Card>
+      ))}
+    </Card.Group>
+  );
+}
