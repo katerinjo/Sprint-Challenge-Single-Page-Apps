148d19b92f7cb090a08a95bb83f5823b4b7bfd59
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-26 09:45:50 -0700

    episodes show

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -2,28 +2,28 @@ import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import { Card } from "semantic-ui-react";
 
-export default function LocationList() {
+export default function EpisodeList() {
   // TODO: Add useState to track data from useEffect
-  const [locations, setLocations] = useState([]);
+  const [episodes, setEpisodes] = useState([]);
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     axios
-      .get("https://rickandmortyapi.com/api/location/3,21")
+      .get("https://rickandmortyapi.com/api/episode/10,28")
       .then(res => {
-        setLocations(res.data);
+        setEpisodes(res.data);
       })
       .catch(console.log);
   }, [])
 
   return <section className='location-list grid-view'>
       <Card.Group>
-        {locations.map(location => (
+        {episodes.map(episode => (
           <Card 
-            header={location.name}
-            meta={location.type}
-            description={`Dimension: ${location.dimension}`}
+            header={episode.episode}
+            meta={episode.air_date}
+            description={episode.name}
           />
         ))}
       </Card.Group>
